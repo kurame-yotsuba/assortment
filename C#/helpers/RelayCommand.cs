288925eaffc678dd8efa4f4bbaf1ffa4ac48bb2e
kurame <kuramechan428@gmail.com>
2021-02-27 14:48:27 +0900

    ObservableとRelayCommandの追加

---
+++ C#/helpers/RelayCommand.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Helpers
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+
+        private readonly Func<bool>? _canExecute;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public RelayCommand(Action execute)
+            : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action execute, Func<bool>? canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? _ = null) => _canExecute is null || _canExecute();
+
+        public void Execute(object? _ = null) => _execute();
+
+        public void OnCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+
+        private readonly Func<T, bool>? _canExecute;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public RelayCommand(Action<T> execute)
+            : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action<T> execute, Func<T, bool>? canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter) => parameter is T param &&  (_canExecute is null || _canExecute(param));
+
+        public void Execute(object? parameter)
+        {
+            if(parameter is T param)
+			{
+                _execute(param);
+            }
+        }
+
+        public void OnCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
